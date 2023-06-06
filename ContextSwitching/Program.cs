ef77e4066c4fd1b3adb89b6518f0b7696a61311d
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2023-06-06 18:01:25 +0200

    Context swithing scenario

--- ContextSwitching/Program.cs
+++ ContextSwitching/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Thread thread = new Thread(WriteUsingNewThread);
+
+            // Worker Thread
+            thread.Start();
+            thread.Name = "MyWorker";
+
+            // Main thread - alays has the UI on it
+            Thread.CurrentThread.Name = "MyMain";
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(" A" + i + " ");
+            }
         }
+
+        private static void WriteUsingNewThread()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(" Z" + i + " ");
+            }
+        }
+
+        // There is no real logic in execution of those two threads. System determines when 
+        // switch the context.
+        // Threading is quite complex.
     }
 }
