37faae9a4c71ce4e38e44b4fcd4d8c7e8445bfef
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2023-06-06 22:33:41 +0200

    Join maethod and IsBackground introduced

---
+++ Concepts/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Concepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(PrintHelloWorld);
+            //Worker thread
+            thread.Start();
+            thread.IsBackground = true;
+
+            // Main Thread
+            // Join called in main thread on worker, causes that main waits till worker finishes.
+            thread.Join();
+            Console.WriteLine("Hello World printed");
+        }
+
+        private static void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+            Thread.Sleep(5000);
+        }
+    }
+}
