10cfdae8b59f028a60901b40263075ce02a8036c
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2023-06-06 18:45:35 +0200

    Shared resources project. At this stage project prints out message twice - in each thread

---
+++ SharedResources/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SharedResources
+{
+    internal class Program
+    {
+        private static bool isCompleted;
+
+        // The message should be printed only once
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(HelloWorld);
+            // Worker Thread
+            thread.Start();
+
+            // Main Thread
+            HelloWorld();
+        }
+
+        private static void HelloWorld()
+        {
+            if (!isCompleted)
+            {
+                Console.WriteLine("Hello World should print only once");
+                isCompleted = true;
+            }
+            
+        }
+    }
+}
