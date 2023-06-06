98883b0d8fff1068ef0d805abcf515cde8101cb0
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2023-06-06 17:25:41 +0200

    Add project files.

---
+++ ContextSwitching/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ContextSwitching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
