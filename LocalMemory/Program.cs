4f8d950549e0cd012b60b574fdd97bea9344bba0
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2023-06-06 19:26:59 +0200

    Local memory demonstartion project

---
+++ LocalMemory/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LocalMemory
+{
+    internal class Program
+    {
+        // There witll be separate copies of local variable i
+        static void Main(string[] args)
+        {
+            // Worker Thread
+            new Thread(PrintOneToThirty).Start();
+
+            // Main Thread
+            PrintOneToThirty();
+        }
+
+        private static void PrintOneToThirty()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Console.Write(i + 1 + " ");
+            }
+        }
+    }
+}
