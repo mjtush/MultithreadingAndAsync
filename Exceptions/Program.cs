cb120f3f946e8be6ca8a97cbc53370b9f39566b5
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2023-06-14 17:50:31 +0200

    Exception handling example added

---
+++ Exceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security;
+using System.Threading;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        // Exception handling is async PartialTrustVisibilityLevel of thread
+        static void Main(string[] args)
+        {
+            //try
+            //{
+                // Worker thread
+                new Thread(Demmo).Start();
+            //}
+            //// Main Thread
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+        }
+
+        private static void Demmo()
+        {   // Handle exceptions in the thread that they can occur
+            try
+            {
+                throw null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
